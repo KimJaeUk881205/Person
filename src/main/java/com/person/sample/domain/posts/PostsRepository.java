2da06feca634c301d8d5c58a463e548e5357c33e
jaeuk <jaeuk@PIONNET08>
2020-06-23 17:21:30 +0900

    추가

---
+++ src/main/java/com/person/sample/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.person.sample.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
---
+++ src/main/java/com/person/sample/service/PostsService.java
@@ -0,0 +1,37 @@
+package com.person.sample.service;
+
+import com.person.sample.domain.posts.Posts;
+import com.person.sample.domain.posts.PostsRepository;
+import com.person.sample.web.dto.PostsResponseDto;
+import com.person.sample.web.dto.PostsSaveRequestDto;
+import com.person.sample.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
+    }
+}
