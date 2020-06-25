dd40e2c73cc1c45190e15290b6cc8d73164d7bd4
jaeuk <jaeuk@PIONNET08>
2020-06-25 18:17:21 +0900

    게시판 기능 완성

---
+++ src/test/java/com/person/sample/web/IndexControllerTest.java
@@ -0,0 +1,34 @@
+package com.person.sample.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩(){
+
+        String body = this.restTemplate.getForObject("/",String.class);
+
+        assertThat(body).contains("스프링부트와 머스테치로 시작하는 웹서비스");
+    }
+
+    @Test
+    public void 글등록_후_수정페이지_이동(){
+
+
+
+    }
+}
