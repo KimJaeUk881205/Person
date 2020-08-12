21acd1fae9ae5a84b3551e1016e81482c34251af
jaeuk <jaeuk@PIONNET08>
2020-08-12 17:28:40 +0900

    진도 진행

--- src/main/java/com/person/sample/web/IndexController.java
+++ src/main/java/com/person/sample/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.person.sample.web;
 
+import com.person.config.auth.LoginUser;
 import com.person.config.auth.dto.SessionUser;
 import com.person.sample.service.PostsService;
 import com.person.sample.web.dto.PostsResponseDto;
@@ -20,10 +21,9 @@ public class IndexController {
 
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
 
-        SessionUser user = (SessionUser)httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName",user.getName());
         }
